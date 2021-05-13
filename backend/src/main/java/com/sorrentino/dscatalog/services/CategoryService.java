c99f8ad8943df3e8fa1ec024e03a7eaac06fadca
SorrentinoFabiano <fabiano.sorrentino@gmail.com>
2021-05-13 09:31:18 -0300

    DTO

--- backend/src/main/java/com/sorrentino/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sorrentino/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.sorrentino.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sorrentino.dscatalog.dto.CategoryDTO;
 import com.sorrentino.dscatalog.entities.Category;
 import com.sorrentino.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,16 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		//Ou.
+		/*List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}*/		
+		//return listDTO;
+		
 	}	
 }
