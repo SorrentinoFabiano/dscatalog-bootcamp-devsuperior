8cb3d7915a701c36541a56c52c6b44f0a44d8177
SorrentinoFabiano <fabiano.sorrentino@gmail.com>
2021-05-13 11:55:26 -0300

    Delete category

--- backend/src/main/java/com/sorrentino/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sorrentino/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sorrentino.dscatalog.dto.CategoryDTO;
 import com.sorrentino.dscatalog.entities.Category;
 import com.sorrentino.dscatalog.repositories.CategoryRepository;
+import com.sorrentino.dscatalog.services.exceptions.DataBaseException;
 import com.sorrentino.dscatalog.services.exceptions.ResourceEntityNotFoundException;
 
 @Service
@@ -61,6 +64,20 @@ public class CategoryService {
 		catch(EntityNotFoundException e){
 			throw new ResourceEntityNotFoundException("Id not found " + id);			
 		}
+	}
+	
+	public void delete(Long id) {
+		try {
+		 	repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceEntityNotFoundException("Id not found " + id);
+		}
+		
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violltion");
+		}
+		
 	}	
 }
 
